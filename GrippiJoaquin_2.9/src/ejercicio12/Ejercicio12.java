b306106f97492371e69d65a7cdecc68814f6ecc9
JoaquinGrippi <joaquinginv@gmail.com>
2025-10-24 20:04:14 +0200

    2_9 Bucles

--- GrippiJoaquin_2.9/src/ejercicio12/Ejercicio12.java
+++ GrippiJoaquin_2.9/src/ejercicio12/Ejercicio12.java
@@ -3,10 +3,31 @@ import java.util.Scanner;
 public class Ejercicio12 {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
 		
-		int valorA, valorB;
-		System.out.print("Introduce un numero: ");
-		valorA = 
+		long resultado, valorA, valorB, contador = 0;
+		
+			System.out.print("Introduce un numero: ");
+			valorA = teclado.nextInt();
+			System.out.print("Introduce su potencia: ");
+			valorB = teclado.nextInt();
+		
+			resultado = valorA;
+		if (valorB < 0) {	
+			long contadorPositivo = valorB * -1, valorC = valorA * -1;
+			
+			for (int i = 1; i < contadorPositivo; i++ ) {
+				resultado = resultado * valorC;
+			}
+			System.out.println("El resultado es: " + resultado);
+		} else {
+			for (int i = 1; i < valorB; i++ ) {
+				resultado = resultado * valorA;
+			}
+			System.out.println("El resultado es: " + resultado);
+		}
+		
+		
 	}
 
 }
